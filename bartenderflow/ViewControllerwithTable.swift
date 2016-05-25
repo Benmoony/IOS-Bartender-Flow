485f85bebd52c403d2cc431b2ed9781257e06720
Miller, Benjamin A <benjaminmiller@cc1-230-403.student.cascadia.edu>
2016-05-25 14:31:33 -0700

    Added flow

---
+++ bartenderflow/ViewControllerwithTable.swift
@@ -0,0 +1,27 @@
+//
+//  ViewControllerwithTable.swift
+//  bartenderflow
+//
+//  Created by Miller, Benjamin A. on 5/25/16.
+//  Copyright © 2016 Miller, Benjamin A. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerwithTable: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    
+    
+}
